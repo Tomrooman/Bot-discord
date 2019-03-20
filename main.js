232cdeb3d47fc87f64ed786dfc87eb0268e9db3e
Tom Rooman <tom.rooman@epitech.eu>
2019-03-21 00:39:17 +0100

    playing song

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
-exports.dispatcher = function(message) {
-    console.log('here 1')
-    console.log('here 2')
-    message.channel.send('Dispatcher')
+const ytdl = require('ytdl-core')
+var connectionObj = false
+exports.dispatcher = function (message) {
+    if (message.content.length > 2) {
+        let words = message.content.split(' ')
+        if (words[0].substr(2, words[0].length - 2) === 'play') {
+            if (words[1]) {
+                playSong(message, words[1])
+            }
+        }
+        if (words[0].substr(2, words[0].length - 2) === 'stop') {
+            if (connectionObj) {
+                console.log('Left channel by stop')
+                connectionObj.channel.leave()
+                connectionObj = false
+            }
+            else {
+                console.log('Not connected in voice channel')
+            }
+        }
+    }
+    else {
+        message.channel.send('Ecrit une commande petit con')
+    }
+
 };
-    
-exports.other = function() {
-    console.log('other 1')
-    console.log('other 2')
+
+var playSong = function (message, url) {
+    let voiceChannel = message.guild.channels
+        .filter(function (channel) { return channel.type === 'voice' })
+        .first()
+    voiceChannel
+        .join()
+        .then(function (connection) {
+            connectionObj = connection
+            let stream = connection.playStream(ytdl(url, { filter: 'audioonly' }))
+            // stream.pause()
+            // stream.resume()
+            stream.setVolume(0.5)
+            stream.on('end', () => {
+                console.log('Left channel by end')
+                connectionObj.channel.leave()
+                connectionObj = false
+
+            })
+            stream.on('error', (err) => {
+                console.log(`Erreur : ${err}`)
+            })
+        })
 };
