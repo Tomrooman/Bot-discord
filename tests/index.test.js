0c140475749ac8fee9e0db17dbd8a0afa21028af
Tom Rooman <tomrooman@hotmail.com>
2019-11-28 21:24:36 +0100

    Modify test & test db connection

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,2 +1,17 @@
-require('./player.test.js')
-require('./message.test.js')
+const MongoClient = require('mongodb').MongoClient
+
+describe('Index', function () {
+    it('Connect to database', function (done) {
+        const url = 'mongodb://localhost:27017/syxbot-database'
+        MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }, function (err, db) {
+            if (err) throw err;
+            global.dbConnection = db.db('syxbot-database')
+            done()
+        });
+    });
+});
+
+require('./user.test.js')
---
+++ tests/user.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('User', function () {
+    it('Futur user test', function () {
+        let test = 1;
+        assert.equal(test, 1)
+    });
+});
