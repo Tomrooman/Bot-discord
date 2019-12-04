72840ba458b7a2959eb2a3eb9f87cbfd27767755
Tom Rooman <tomrooman@hotmail.com>
2019-12-04 22:01:30 +0100

    add many radio

--- assets/js/player.js
+++ assets/js/player.js
@@ -20,7 +20,35 @@ const waitArray = []
 const nextSetLoop = []
 const isPlaying = []
 const tryToNext = []
-const radioAvailable = ['nrj', 'subarashii']
+const radioAvailable = [
+    'NRJ',
+    'Subarashii',
+    'Bel-RTL',
+    'Contact',
+    'Nostalgie-BE',
+    'Nostalgie-FR',
+    'Classic21',
+    'Pure-FM',
+    'Musiq3',
+    'VivaCite',
+    'Fun-radio',
+    'Rire&chanson',
+    'Virgin',
+    'RFM',
+    'RMC',
+    'BFM-Business',
+    'jazz',
+    'Cherie-FM',
+    'Europe1',
+    'RTL',
+    'RTL2',
+    'Classique',
+    'Skyrock',
+    'France-Inter',
+    'France-Culture',
+    'France-Musique',
+    'France-Bleu'
+]
 
 function playSongs(message, command, words) {
     const voiceChannel = Helper.take_user_voiceChannel(message)
@@ -104,7 +132,7 @@ function youtubeResearch(message, string, voiceChannel, type, nextPage = false) 
             }
         }
         else {
-            console.log('Erreur')
+            console.log('Error search by title no results')
         }
     })
 }
@@ -170,9 +198,11 @@ function sendSearchResultsAsString(message, voiceChannel, type) {
 function selectSongOrPlaylistInSearchList(message, words) {
     const userChannel = Helper.take_user_voiceChannel(message)
     if (words[1] === 'p' || words[1] === 'play') {
+        // If there is something after p|play search for the music
         if (words[2]) {
             selectSongInSearchList(message, parseInt(words[2]))
         }
+        // If nothing after p|play send the list
         else if (userChannel) {
             if (Helper.verifyBotLocation(message, userChannel)) {
                 sendSearchResultsAsString(message, userChannel, 'video')
@@ -183,9 +213,11 @@ function selectSongOrPlaylistInSearchList(message, words) {
         }
     }
     else if (words[1] === 'pl' || words[1] === 'playlist') {
+        // If there is something after pl|playlist search for the playlist
         if (words[2]) {
             selectSongInSearchList(message, parseInt(words[2]), 'playlist')
         }
+        // If nothing after pl|playlist send the list
         else if (userChannel) {
             if (Helper.verifyBotLocation(message, userChannel)) {
                 sendSearchResultsAsString(message, userChannel, 'playlist')
@@ -277,15 +309,20 @@ function makeAndSendSearchListArray(message, userChannel, musicExist, playlistEx
 
 function playSong(message, connection, retry = false) {
     const userChannel = Helper.take_user_voiceChannel(message)
+    if (!retry) {
+        sendMusicEmbed(message, playlistInfos[userChannel.id][0].title, playlistInfos[userChannel.id][0].id, [false, 1])
+    }
     isPlaying[userChannel.id] = true
     delete tryToNext[userChannel.id]
     connectionsArray[userChannel.id] = connection
     const stream = ytdl(playlistArray[userChannel.id][0], { filter: 'audio', liveBuffer: 10000 })
     streamsArray[userChannel.id] = connection.play(stream)
     streamsArray[userChannel.id].setVolume(0.4)
-    if (!retry) {
-        sendMusicEmbed(message, playlistInfos[userChannel.id][0].title, playlistInfos[userChannel.id][0].id, [false, 1])
-    }
+    // streamsArray[userChannel.id].on('error', err => {
+    //     console.log('\nError')
+    //     console.log('Musique : ', playlistInfos[userChannel.id][0].title)
+    //     console.log('Erreur : ', err)
+    // })
     streamsArray[userChannel.id].on('finish', () => {
         setTimeout(() => {
             setArrays(message)
@@ -506,7 +543,7 @@ function clearAndAddArrayInfos(voiceChannel, infos, clear = true) {
 function radioExist(radioCheck) {
     let checkExist = false
     radioAvailable.map(r => {
-        if (r === radioCheck) {
+        if (r.toLowerCase() === radioCheck) {
             checkExist = true
         }
     })
@@ -519,6 +556,81 @@ function getRadioLink(radioForLink) {
     }
     else if (radioForLink === 'subarashii') {
         return 'http://listen.radionomy.com/subarashii.mp3'
+    }
+    else if (radioForLink === 'bel-rtl') {
+        return 'http://belrtl.ice.infomaniak.ch/belrtl-mp3-128.mp3'
+    }
+    else if (radioForLink === 'contact') {
+        return 'http://broadcast.infomaniak.ch/radiocontact-mp3-192.mp3'
+    }
+    else if (radioForLink === 'nostalgie-be') {
+        return 'http://streamingp.shoutcast.com/NostalgiePremium-mp3'
+    }
+    else if (radioForLink === 'nostalgie-fr') {
+        return 'http://cdn.nrjaudio.fm/audio1/fr/30601/mp3_128.mp3?origine=fluxradios'
+    }
+    else if (radioForLink === 'classic21') {
+        return 'http://radios.rtbf.be/classic21-128.mp3'
+    }
+    else if (radioForLink === 'pure-fm') {
+        return 'http://radios.rtbf.be/pure-128.mp3'
+    }
+    else if (radioForLink === 'musiq3') {
+        return 'http://radios.rtbf.be/musiq3-128.mp3'
+    }
+    else if (radioForLink === 'vivacite') {
+        return 'http://radios.rtbf.be/vivabxl-128.mp3'
+    }
+    else if (radioForLink === 'fun-radio') {
+        return 'http://streaming.radio.funradio.fr/fun-1-44-128'
+    }
+    else if (radioForLink === 'rire&chanson') {
+        return 'http://cdn.nrjaudio.fm/audio1/fr/30401/mp3_128.mp3?origine=fluxradios'
+    }
+    else if (radioForLink === 'virgin') {
+        return 'http://vr-live-mp3-128.scdn.arkena.com/virginradio.mp3'
+    }
+    else if (radioForLink === 'rfm') {
+        return 'http://rfm-live-mp3-128.scdn.arkena.com/rfm.mp3'
+    }
+    else if (radioForLink === 'rmc') {
+        return 'http://rmc.bfmtv.com/rmcinfo-mp3'
+    }
+    else if (radioForLink === 'bfm-business') {
+        return 'http://chai5she.cdn.dvmr.fr/bfmbusiness'
+    }
+    else if (radioForLink === 'jazz') {
+        return 'http://jazzradio.ice.infomaniak.ch/jazzradio-high.mp3'
+    }
+    else if (radioForLink === 'cherie-fm') {
+        return 'http://cdn.nrjaudio.fm/audio1/fr/30201/mp3_128.mp3?origine=fluxradios'
+    }
+    else if (radioForLink === 'europe1') {
+        return 'http://mp3lg4.tdf-cdn.com/9240/lag_180945.mp3'
+    }
+    else if (radioForLink === 'rtl') {
+        return 'http://streaming.radio.rtl.fr/rtl-1-44-128'
+    }
+    else if (radioForLink === 'rtl2') {
+        return 'http://streaming.radio.rtl2.fr/rtl2-1-44-128'
+    }
+    else if (radioForLink === 'classique') {
+        return 'http://radioclassique.ice.infomaniak.ch/radioclassique-high.mp3'
+    }
+    else if (radioForLink === 'skyrock') {
+        return 'http://www.skyrock.fm/stream.php/tunein16_128mp3.mp3'
+    }
+    else if (radioForLink === 'france-inter') {
+        return 'http://direct.franceinter.fr/live/franceinter-midfi.mp3'
+    }
+    else if (radioForLink === 'france-culture') {
+        return 'http://direct.franceculture.fr/live/franceculture-midfi.mp3'
+    }
+    else if (radioForLink === 'france-musique') {
+        return 'http://direct.francemusique.fr/live/francemusique-midfi.mp3'
+    }
+    else if (radioForLink === 'france-bleu') {
+        return 'http://direct.francebleu.fr/live/fbpicardie-midfi.mp3'
     }
 }
 
