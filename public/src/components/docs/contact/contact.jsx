4d4211ba15a24e387973167708e3702505f972e0
Tom Rooman <tomrooman@hotmail.com>
2020-01-09 16:22:12 +0100

    add error/success contact message

--- public/src/components/docs/contact/contact.jsx
+++ public/src/components/docs/contact/contact.jsx
@@ -3,12 +3,11 @@
 import React from 'react';
 import $ from 'jquery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
-
-library.add(faInfoCircle);
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+library.add(faTimesCircle);
 
 export default class Contact extends React.Component {
     constructor() {
@@ -20,19 +19,22 @@ export default class Contact extends React.Component {
         };
     }
 
+    componentDidMount() {
+        $('.alert')[0].style.display = 'none';
+    }
+
     sendMessage() {
         if (this.state.mail.length >= 1 && this.state.object.length >= 1 && this.state.message.length >= 1) {
+            $('.alert')[0].style.display = 'none';
+            $('.contact-submit-div')[0].children[0].innerHTML = 'Envoi du message en cours ...';
+            $('.contact-submit-div button')[0].style.opacity = '';
+            $('.contact-submit-div button')[0].style.cursor = '';
+            $('.alert')[0].className = 'contact alert';
             this.setState({
                 mail: '',
                 object: '',
                 message: ''
             });
-            $('.contact-submit-div button')[0].style.opacity = '';
-            $('.contact-submit-div button')[0].style.cursor = '';
-            $('input')[0].value = '';
-            $('input')[1].value = '';
-            $('textarea')[0].value = '';
-            $('.contact-submit-div')[0].children[0].innerHTML = 'Envoi du message en cours ...';
             axios.post('/api/docs/contact', {
                 mail: this.state.mail,
                 object: this.state.object,
@@ -40,9 +42,36 @@ export default class Contact extends React.Component {
             })
                 .then(res => {
                     $('.contact-submit-div')[0].children[0].innerHTML = 'Envoyer mon message';
-                    console.log('Respond : ', res);
+                    if (res.data) {
+                        $('input')[0].value = '';
+                        $('input')[1].value = '';
+                        $('textarea')[0].value = '';
+                        $('.alert').addClass('alert-success');
+                        $('.alert').html('<span>' + $('.alert')[0].children[0].innerHTML + '</span><span style="display: none">' + $('.alert')[0].children[1].innerHTML + '</span> Message envoyé !');
+                        $('.alert')[0].style.display = '';
+                    }
+                    else {
+                        this.handleError();
+                    }
+                })
+                .catch(() => {
+                    $('.contact-submit-div')[0].children[0].innerHTML = 'Envoyer mon message';
+                    this.handleError();
                 });
         }
+    }
+
+    handleError() {
+        this.setState({
+            mail: $('input')[0].value,
+            object: $('input')[1].value,
+            message: $('textarea')[0].value
+        });
+        $('.contact-submit-div button')[0].style.opacity = '1';
+        $('.contact-submit-div button')[0].style.cursor = 'pointer';
+        $('.alert').addClass('alert-danger');
+        $('.alert').html('<span style="display: none">' + $('.alert')[0].children[0].innerHTML + '</span><span>' + $('.alert')[0].children[1].innerHTML + '</span> Une erreur est survenue, veuillez réessayer ou me contacter à cette adresse : syxbot@hotmail.com');
+        $('.alert')[0].style.display = '';
     }
 
     handleOnChange(type, e) {
@@ -68,8 +97,9 @@ export default class Contact extends React.Component {
                 <h1><FontAwesomeIcon icon='pen-fancy' /> Me contacter</h1>
                 <div className='top_logo'><img src='/img/Syxbot_logo.png' /></div>
                 <div className='docs_content'>
-                    <div className='docs_panel warning'>
-                        <p><FontAwesomeIcon icon='info-circle' /> Tous les champs doivent être remplis pour envoyer le message.</p>
+                    <div className='contact alert'>
+                        <span className='success-icon'><FontAwesomeIcon icon='check' /></span>
+                        <span className='error-icon'><FontAwesomeIcon icon='times-circle' /></span>
                     </div>
                     <div className='docs-contact'>
                         <h3>Formulaire</h3>
